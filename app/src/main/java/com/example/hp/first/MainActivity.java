02fc95dad47399245b36d76456693bf27f6b2f14
Deathsorrow <976499001@qq.com>
2019-03-26 21:13:43 +0800

    first

--- app/src/main/java/com/example/hp/first/MainActivity.java
+++ app/src/main/java/com/example/hp/first/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.hp.first;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import static com.example.hp.first.R.id.btn;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView out;
+    EditText edit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        out= findViewById(R.id.tetout);
+        out.setText(R.string.shuchu);
+        edit=findViewById(R.id.inp);
+        Button btn=findViewById(R.id.btn);
+        btn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        String str=edit.getText().toString();
+        Double num=Double.parseDouble(str);
+        Double hsd=num*33.8;
+        new java.text.DecimalFormat("#.00").format(hsd);
+        String  result = String.valueOf(hsd);
+        out.setText(result+"华氏度");
     }
 }
