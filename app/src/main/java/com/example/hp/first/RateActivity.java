de790f76b78725ad2dbf15634f33e9561be089f9
Deathsorrow <976499001@qq.com>
2019-04-02 21:08:03 +0800

    first

---
+++ app/src/main/java/com/example/hp/first/RateActivity.java
@@ -0,0 +1,21 @@
+package com.example.hp.first;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class RateActivity extends AppCompatActivity {
+    EditText rmb;
+    TextView show;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rate);
+        rmb=findViewById(R.id.rmb);
+        show=findViewById(R.id.showout);
+
+    }
+    public void OnClick
+
+}
