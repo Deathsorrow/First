1aad7c0bf4bb94c771626de93a8f50e27a8081cc
Deathsorrow <976499001@qq.com>
2019-05-08 16:59:47 +0800

    first

--- app/src/main/java/com/example/hp/first/MyList2Activity.java
+++ app/src/main/java/com/example/hp/first/MyList2Activity.java
@@ -1,6 +1,7 @@
 package com.example.hp.first;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -8,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -19,10 +21,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import static android.content.ContentValues.TAG;
+public class MyList2Activity extends ListActivity implements Runnable,AdapterView.OnItemClickListener {
 
-public class MyList2Activity extends ListActivity implements Runnable{
-
+    private  String TAG = "mylist2";
     Handler handler;
     private ArrayList<HashMap<String, String>> listItems;  //存放文字、图片信息
     private SimpleAdapter listItemAdapter;  //适配器
@@ -54,12 +55,7 @@ public class MyList2Activity extends ListActivity implements Runnable{
                 super.handleMessage(msg);
             }
         };
-        getListView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-            }
-        });
+        getListView().setOnItemClickListener(this);
     }
     private void initListView() {
         listItems = new ArrayList<HashMap<String, String>>();
@@ -114,5 +110,32 @@ public class MyList2Activity extends ListActivity implements Runnable{
         Message msg = handler.obtainMessage(7);
         msg.obj = retList;
         handler.sendMessage(msg);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        Log.i(TAG, "onItemClick: parent" + parent);
+        Log.i(TAG, "onItemClick: view" + view);
+        Log.i(TAG, "onItemClick: position" + position);
+        Log.i(TAG, "onItemClick: id" + id);
+        HashMap<String,String> map = (HashMap<String, String>) getListView().getItemAtPosition(position);
+        String titleStr = map.get("ItemTitle");
+        String detailStr = map.get("ItemDetail");
+        Log.i(TAG, "onItemClick: titleStr" + titleStr);
+        Log.i(TAG, "onItemClick: detailStr" + detailStr);
+
+        TextView title = view.findViewById(R.id.itemTitle);
+        TextView detail = view.findViewById(R.id.itemDetail);
+        String title2 = String.valueOf(title.getText());
+        String detail2 = String.valueOf(detail.getText());
+        Log.i(TAG, "onItemClick: title2" + title2);
+        Log.i(TAG, "onItemClick: detail2" + detail2);
+
+        //打开新的页面，传入参数
+        Intent rateCalc = new Intent(this,RateCalcActivity.class);
+        rateCalc.putExtra("title",titleStr);
+        rateCalc.putExtra("rate",Float.parseFloat(detailStr));
+        startActivity(rateCalc);
     }
 }
